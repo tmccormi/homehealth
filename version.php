fc34d56f732af5b983a9051d0bfada8e76f586af
Tony McCormick <tony@mi-squared.com>
2013-08-28 13:54:01 -0700

    [MEDASKO-12] increment datbase version to 76

--- version.php
+++ version.php
@@ -6,7 +6,7 @@
 $v_major = '4';
 $v_minor = '1';
 $v_patch = '1';
-$v_tag   = '-dev'; // minor revision number, should be empty for production releases
+$v_tag   = '-hlh'; // minor revision number, should be empty for production releases
 
 // A real patch identifier. This is incremented when release a patch for a
 // production release. Not the above $v_patch variable is a misnomer and actually
@@ -17,5 +17,6 @@ $v_realpatch = '0';
 // is a database change in the course of development.  It is used
 // internally to determine when a database upgrade is needed.
 //
-$v_database = 75;
+// THIS VERSION NUMBER IS MEDASKO specific, be careful if we merge with OpenEMR upstream (tm)
+$v_database = 76;
 ?>
